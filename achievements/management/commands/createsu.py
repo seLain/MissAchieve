3f2d05764c34feb7ae9ca2451f0c6692204e2842
seLain <selain@nature.ee.ncku.edu.tw>
2018-04-03 16:20:09 +0800

    move default admin settings to achievements\secret_settings
    the secret_settings file can be generated by an empty settings.example.py

--- achievements/management/commands/createsu.py
+++ achievements/management/commands/createsu.py
@@ -2,6 +2,20 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 
 class Command(BaseCommand):
-    def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "admin@admin.com", "admin")
+	def handle(self, *args, **options):
+		try:
+			from achievements import secret_settings as settings
+			if not User.objects.filter(username=settings.DEFAULT_ADMIN).exists():
+				User.objects.create_superuser(settings.DEFAULT_ADMIN, settings.DEFAULT_ADMIN_PASSWORD, settings.DEFAULT_ADMIN_MAIL)
+		except ImportError:
+			# [todo] add log to this exception and give instrucitons
+			print('Oops. Can not import achievements.secret_settings')
+			print('Please check if achievements.settings.py exists.')
+			print('No admin superuser created.')
+			pass
+		except AttributeError:
+			# [todo] add log to this exception and give instrucitons
+			print('Oops. Can not find needed settings in achievements.secret_settings.py')
+			print('Please refer to achievements.settings.example.py')
+			print('No admin superuser created.')
+			pass
---
+++ achievements/settings.example.py
@@ -0,0 +1,6 @@
+"""
+Settings for achievements app.
+"""
+DEFAULT_ADMIN = ''
+DEFAULT_ADMIN_PASSWORD = ''
+DEFAULT_ADMIN_MAIL = ''
